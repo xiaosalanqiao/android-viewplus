f1adb7bb0a29b43a7e6c49e38919acfe4e10a01f
zhaojin <jiiiiiin@yeah.net>
2019-06-07 15:47:41 +0800

    refactor: 修复获取设备唯一标识因为sdk升级导致的问题 && 升级依赖

--- vplus-core/src/main/java/cn/jiiiiiin/vplus/core/util/device/DeviceUtil.java
+++ vplus-core/src/main/java/cn/jiiiiiin/vplus/core/util/device/DeviceUtil.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.telephony.TelephonyManager;
